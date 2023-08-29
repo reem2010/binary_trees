c7cb1265c0a3c28403604d97054df7d9359c6aaa
reem <tarekreem950@gmail.com>
2023-08-29 15:39:10 -0700

    delete node

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * void binary_tree_delete - deletes an entire binary tree
+ * @tree: entire binary tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
