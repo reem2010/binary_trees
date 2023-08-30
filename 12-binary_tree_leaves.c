e3b0da8f9d2de72c409ea7e3aed75cbb358b7907
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-08-30 09:07:44 -0700

    Leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves -  measures the leaves of a binary tree
+ * @tree: pointer to the root node of the tree to measeight.
+ * Return: leaves of a binary tree or NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (!tree)
+		return (0);
+	leaves += binary_tree_is_leaf(tree);
+	leaves += binary_tree_leaves(tree->right);
+	leaves += binary_tree_leaves(tree->left);
+	return (leaves);
+}
