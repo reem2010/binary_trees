e73a6aa37e878b4bc55353c43198684f664cfb3d
reem <tarekreem950@gmail.com>
2023-08-30 10:52:27 -0700

    sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,11 +2,11 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: the node
- * Retuen: sibling of a node
+ * Return: sibling of a node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node ||!node->parent)
+	if (!node || !node->parent)
 		return (NULL);
 	if (node == node->parent->left)
 		return (node->parent->right);
